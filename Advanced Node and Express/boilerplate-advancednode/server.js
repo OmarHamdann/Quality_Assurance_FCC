95ff77fbdb0406d6b70fa6c68bc71e236603aceb
OmarHamdann <omar.haamdan@gmail.com>
2022-09-18 09:54:30 +0300

    serialize and deserialize user using passport

--- Advanced Node and Express/boilerplate-advancednode/server.js
+++ Advanced Node and Express/boilerplate-advancednode/server.js
@@ -6,11 +6,29 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 //pug
 const pug = require('pug');
 const app = express();
+//passport and session
+const passport = require('passport');
+const session = require('express-session');
+//connect-mongo
+const ObjectID = require('mongodb').ObjectID;
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+//session and passport setup
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 
 //pug setup
 app.set('view engine', 'pug');
@@ -26,4 +44,19 @@ app.route('/').get((req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
+});
+
+
+
+//save userId to cookie
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+//retrieve userId from cookie
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) },
+   (err, doc) => {
+    done(null, null);
+  });
 });
