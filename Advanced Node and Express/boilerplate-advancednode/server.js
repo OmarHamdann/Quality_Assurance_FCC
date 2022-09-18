041cdf206ddd34765e37a649f18bd03b0cc5e9fb
OmarHamdann <omar.haamdan@gmail.com>
2022-09-18 16:12:09 +0300

    using localstratagy passport and create middleware

--- Advanced Node and Express/boilerplate-advancednode/server.js
+++ Advanced Node and Express/boilerplate-advancednode/server.js
@@ -6,9 +6,12 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'pug');
+app.set('views','./views/pug');
 
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -31,9 +34,10 @@ myDB(async (client) => {
   // Be sure to change the title
   app.route('/').get((req, res) => {
     // Change the response to render the Pug template
-    res.render('pug', {
+    res.render('index', {
       title: 'Connected to Database',
-      message: 'Please login'
+      message: 'Please login',
+      showLogin: true
     });
   });
 
@@ -46,12 +50,51 @@ myDB(async (client) => {
       done(null, doc);
     });
   });
+
+  const findUserDocumnet = new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log('User ' + username + ' attempted to log in.');
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  });
+
+  passport.use(findUserDocumnet);
+   
+
   // Be sure to add this...
 }).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('pug', { title: e, message: 'Unable to login' });
+    res.render('index', { title: e, message: 'Unable to login' });
   });
 });
+
+console.log(passport)
+
+app.post('/login',
+ bodyParser.urlencoded({ extended: false }),
+ passport.authenticate('local', { failureRedirect: '/' }), 
+ (req, res) => {
+  res.redirect('/profile');
+});
+
+// Be sure to add this... (middleware) to make sure the user is authenticated when calling the /profile route 
+const isSignedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+
+app.get('/profile', isSignedIn , (req, res) => {
+  res.render('profile', { username: req.user.name });
+});
+
+
+
 // app.listen out here...
 
 app.listen(process.env.PORT || 3000, () => {
