51777fca0a530399e7d3990e7661bfb7fdffca89
OmarHamdann <omar.haamdan@gmail.com>
2022-09-22 13:01:30 +0300

    print online user

--- Advanced Node and Express/boilerplate-advancednode/public/client.js
+++ Advanced Node and Express/boilerplate-advancednode/public/client.js
@@ -2,12 +2,16 @@
 $(document).ready(function () {
   /* Global io */
   let socket = io();
+  // change the text of the h1 tag to 'Connected!' when the client is connected to the server
+  socket.on('user count', function (data) {
+    console.log(data);
+  });
 
   // Form submittion with new message in field with id 'm'
-  $("form").submit(function () {
-    let messageToSend = $("#m").val();
+  $('form').submit(function () {
+    let messageToSend = $('#m').val();
     // Send message to server here?
-    $("#m").val("");
+    $('#m').val('');
     return false; // Prevent form submit from refreshing page
   });
 });
